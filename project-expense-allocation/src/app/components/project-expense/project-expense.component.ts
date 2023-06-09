845a60411f2fd8ca138f4c88ed081589aa152401
Shameem Alam <theshameem.alam@gmail.com>
2023-06-09 13:04:19 +0600

    feat: one level employee add

--- project-expense-allocation/src/app/components/project-expense/project-expense.component.ts
+++ project-expense-allocation/src/app/components/project-expense/project-expense.component.ts
@@ -3,8 +3,85 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-project-expense',
   templateUrl: './project-expense.component.html',
-  styleUrls: ['./project-expense.component.scss']
+  styleUrls: ['./project-expense.component.scss'],
 })
 export class ProjectExpenseComponent {
+  employees: any = [];
+  totalCost: number = 0;
 
+  // employeeList: any = [
+  //   {
+  //     Name: 'PM',
+  //     EmployeeList: [
+  //       {
+  //         Name: 'QA',
+  //       },
+  //       {
+  //         Name: 'DEV',
+  //       },
+  //       {
+  //         Name: 'PM',
+  //         EmployeeList: [
+  //           {
+  //             Name: 'QA',
+  //           },
+  //           {
+  //             Name: 'DEV',
+  //           },
+  //           {
+  //             Name: 'PM',
+  //             EmployeeList: [
+  //               {
+  //                 Name: 'QA',
+  //               },
+  //               {
+  //                 Name: 'DEV',
+  //               },
+  //             ],
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     Name: 'QA',
+  //   },
+  //   {
+  //     Name: 'DEV',
+  //   },
+  // ];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  resetCalculation(): void {
+    console.log('Reset clicked!');
+    this.employees = [];
+    this.totalCost = 0;
+  }
+
+  addProjectManager(index?: number): void {
+    this.employees.push({
+      Name: 'PM',
+      Cost: 300,
+    });
+    this.totalCost += 300;
+  }
+
+  addDeveloper(index?: number): void {
+    this.employees.push({
+      Name: 'Developer',
+      Cost: 1000,
+    });
+    this.totalCost += 1000;
+  }
+
+  addQualityAssurance(index?: number): void {
+    this.employees.push({
+      Name: 'QA',
+      Cost: 500,
+    });
+    this.totalCost += 500;
+  }
 }
